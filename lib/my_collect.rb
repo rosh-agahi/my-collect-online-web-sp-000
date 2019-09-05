ce26cfe979ae1d345ef3860d507732eaa6dcd0ea
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 18:06:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(array)
   i=0 
-  while i<array.length
   collection = []
-  collection << yield (array[i])
+  while i<array.length
+  
+  collection << yield(array[i])
   i +=1 
 end
 collection 
